a93969f542857a331280a82d5926ca5e4d3522f4
Mike Chiappe <33669760+Mimi8298@users.noreply.github.com>
2024-02-18 19:19:02 +0100

    Update IsExternalInit.cs

--- Runtime/IsExternalInit.cs
+++ Runtime/IsExternalInit.cs
@@ -1,4 +1,4 @@
-#if false == UNITY_2022_1_OR_NEWER
+#if NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP2_0 || NETCOREAPP2_1 || NETCOREAPP2_2 || NETCOREAPP3_0 || NETCOREAPP3_1 || NET45 || NET451 || NET452 || NET6 || NET461 || NET462 || NET47 || NET471 || NET472 || NET48
 
 // NOTE: to use record & init accessor in Unity 2021
 //       https://stackoverflow.com/questions/62648189/testing-c-sharp-9-0-in-vs2019-cs0518-isexternalinit-is-not-defined-or-imported
